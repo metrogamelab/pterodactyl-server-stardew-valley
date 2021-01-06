dac4ba32162b75713c236eb5f2fdbac3e0ba9001
William Gladstone <will@jollypebble.com>
2021-01-06 15:51:34 -0500

    update install script

--- install_script.sh
+++ install_script.sh
@@ -51,18 +51,18 @@ mkdir -p ./logs
 wget https://github.com/Pathoschild/SMAPI/releases/download/3.8/SMAPI-3.8.0-installer.zip -qO ./storage/nexus.zip
 unzip ./storage/nexus.zip -d ./nexus/
 /bin/bash -c "echo -e \"2\n/mnt/server\n1\n\" | /usr/bin/mono /mnt/server/nexus/SMAPI\ 3.8.0\ installer/internal/unix-install.exe"
-wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-stardew-valley-server/stardew_valley_server.config -qO ./storage/stardew_valley_server.config
-wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-stardew-valley-server/i3.config -qO ./config
-wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-stardew-valley-server/alwayson.zip -qO ./storage/alwayson.zip
-wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-stardew-valley-server/unlimitedplayers.zip -qO ./storage/unlimitedplayers.zip
-wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-stardew-valley-server/autoloadgame.zip -qO ./storage/autoloadgame.zip
+wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-server-stardew-valley/main/stardew_valley_server.config -qO ./storage/stardew_valley_server.config
+wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-server-stardew-valley/main/i3.config -qO ./config
+wget https://github.com/metrogamelab/pterodactyl-server-stardew-valley/raw/main/alwayson.zip -qO ./storage/alwayson.zip
+wget https://github.com/metrogamelab/pterodactyl-server-stardew-valley/raw/main/unlimitedplayers.zip -qO ./storage/unlimitedplayers.zip
+wget https://github.com/metrogamelab/pterodactyl-server-stardew-valley/raw/main/autoloadgame.zip -qO ./storage/autoloadgame.zip
 unzip ./storage/alwayson.zip -d ./Mods
 unzip ./storage/unlimitedplayers.zip -d ./Mods
 unzip ./storage/autoloadgame.zip -d ./Mods
-wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-stardew-valley-server/alwayson.json -qO ./Mods/Always On Server/config.json
-wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-stardew-valley-server/unlimitedplayers.json -qO ./Mods/UnlimitedPlayers/config.json
-wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-stardew-valley-server/autoloadgame.json -qO ./Mods/AutoLoadGame/config.json
-wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-stardew-valley-server/stardew-valley-server.sh -qO ./stardew-valley-server.sh
+wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-server-stardew-valley/main/alwayson.json -qO ./Mods/Always On Server/config.json
+wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-server-stardew-valley/main/unlimitedplayers.json -qO ./Mods/UnlimitedPlayers/config.json
+wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-server-stardew-valley/main/autoloadgame.json -qO ./Mods/AutoLoadGame/config.json
+wget https://raw.githubusercontent.com/metrogamelab/pterodactyl-server-stardew-valley/main/stardew-valley-server.sh -qO ./stardew-valley-server.sh
 chmod +x ./stardew-valley-server.sh 
 rm ./storage/alwayson.zip ./storage/unlimitedplayers.zip ./storage/autoloadgame.zip
 
